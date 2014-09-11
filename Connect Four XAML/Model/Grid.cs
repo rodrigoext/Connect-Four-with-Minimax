daac8af934caeb1bf6ebc7e3fc69ce7b330fd70d
rodrigoext@outlook.com <rodrigo12345,.>
2014-09-10 21:53:51 -0700

    Removed Project test

---
+++ Connect Four XAML/Model/Grid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect_Four_XAML.Model
+{
+    class Grid
+    {
+        int _rows = 6;
+        int _columns = 7;
+
+        public Grid()
+        {
+
+        }
+    }
+}
---
+++ Connect Four XAML/Model/Node.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect_Four_XAML.Model
+{
+    class Node
+    {
+        private bool _terminal;
+        private List<Node> _children;
+
+        public Node()
+        {
+            _children = new List<Node>();
+            _terminal = false;
+        }
+
+        public void setTerminal(bool isTerminal)
+        {
+            _terminal = isTerminal;
+        }
+
+        public bool isTerminal()
+        {
+            return _terminal;
+        }
+    }
+}
